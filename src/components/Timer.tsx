4e131aea709bd588391840d27636d9cec746f6aa
David Johrden <david.johrden@gmail.com>
2024-07-31 10:27:49 +0200

    update: prevent negative values and zero in input for timer start

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -16,13 +16,16 @@ function Timer({ isLightMode }: TimerProps) {
   const [stop, setStop] = useState(false);
   const [isRunning, setIsRunning] = useState(false);
   const intervalId = useRef<number | undefined>(undefined);
-  const intervalRefreshTime = 10; // in Millisekunden
+  const intervalRefreshTime = 50; // in Millisekunden
   let secondsLeft = inputValue - (now - startTime) / 1000;
 
   const handleInputChangeEvent = (
     event: React.ChangeEvent<HTMLInputElement>
   ) => {
-    setInputValue(Number(event.target.value));
+    const newInputValue = Number(event.target.value);
+    if (newInputValue >= 0) {
+      setInputValue(newInputValue);
+    } else alert("Negative values are not allowed");
   };
 
   function startIntervalProcess() {
@@ -40,6 +43,10 @@ function Timer({ isLightMode }: TimerProps) {
   }, [startTime, now, inputValue]);
 
   function handleStart() {
+    if (inputValue === 0) {
+      alert("The timer cannot be started with zero.");
+      return;
+    }
     if (!isRunning) {
       clearInterval(intervalId.current);
       if (!stop) {
@@ -48,11 +55,11 @@ function Timer({ isLightMode }: TimerProps) {
         setNow(dateNow);
         startIntervalProcess();
       } else {
-        const timeElapsed = now - startTime; // speichert die abgelaufene Zeit von inputValue
-        const dateNow = Date.now(); // speichern der aktuellen Zeit
-        setNow(dateNow); // setzen der aktuellen Zeit
-        setStartTime(dateNow - timeElapsed); // setzen der Zeit, die bereits abgelaufen wäre
-        startIntervalProcess(); // starten der "neuen Zeit"
+        const timeElapsed = now - startTime;
+        const dateNow = Date.now();
+        setNow(dateNow);
+        setStartTime(dateNow - timeElapsed);
+        startIntervalProcess();
         setStop(false);
       }
       setIsRunning(true);
@@ -60,17 +67,17 @@ function Timer({ isLightMode }: TimerProps) {
   }
 
   function handleReset() {
-    clearInterval(intervalId.current); // stopt den aktuellen Intervall
-    setNow(0); // Resettet den now
-    setStartTime(0); // Resettet die startTime auf 0
-    setStop(false); // Setzt den Stop
-    setIsRunning(false); // Setzt isRunning auf false
+    clearInterval(intervalId.current);
+    setNow(0);
+    setStartTime(0);
+    setStop(false);
+    setIsRunning(false);
   }
 
   function handleStop() {
-    clearInterval(intervalId.current); // stoppt das weitere Herunterzählen
-    setStop(true); // gibt an, dass die Pause gedrückt wurde
-    setIsRunning(false); // Setze isRunning auf false
+    clearInterval(intervalId.current);
+    setStop(true);
+    setIsRunning(false);
   }
 
   return (
